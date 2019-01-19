811ce99a44df653ad88a2a5d139c7c9addbcd92a
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-01-19 21:22:58 +0700

    object

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,13 @@ public class Main {
     public enum seasion{mua_xuan,mua_he,mua_thu,mua_dong};
 
     public static void main(String[] args) {
-        //System.out.println("Hello World! 2");
-
-        Main x = new Main();
-        //x.he_so = 100;
-       // x.he_so_mac_dinh = 20;
-        if(x.he_so == 100){
-            System.out.println("he so la: "+ x.he_so + " va he so mac dinh: "+ seasion.mua_xuan);
-        }else if(x.he_so == 20){
-            System.out.println("he so la: "+ x.he_so + " va he so mac dinh: "+ seasion.mua_he);
-        }else{
-            System.out.println("he so la: "+ x.he_so + " va he so mac dinh: "+ seasion.mua_dong);
-        }
-        Scanner scanner = new Scanner(System.in);
-
-        switch (x.he_so){
-            case 100: System.out.println("he so 100");break;
-            default: System.out.println("he so khac");
-        }
-
-    }
-
-    public int tinh_gt(){
-        return  0;
+       SoNguyen objSN = new SoNguyen();
+       objSN.nhapGiaTri();
+       System.out.println("GT: "+objSN.tinhGiaiThua());
+       objSN.nhapSoThuNhat();
+       objSN.nhapSoThuHai();
+        System.out.println("Tong: "+objSN.tinhTong());
+        System.out.println("Tong: "+objSN.tinhTong());
+        System.out.println("Tong: "+objSN.tinhTong());
     }
 }
---
+++ src/SoNguyen.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SoNguyen{
+
+    public int so_can_tinh;
+
+    public int so_thu_nhat;
+    public int so_thu_hai;
+
+    public void nhapGiaTri(){
+        System.out.println("Nhap n=");
+        Scanner scanner = new Scanner(System.in);
+        this.so_can_tinh = scanner.nextInt();
+    }
+
+    public void nhapSoThuNhat(){
+        System.out.println("Nhap so thu nhat:");
+        Scanner scanner = new Scanner(System.in);
+        this.so_thu_nhat = scanner.nextInt();
+    }
+
+    public void nhapSoThuHai(){
+        System.out.println("Nhap so thu hai:");
+        Scanner scanner = new Scanner(System.in);
+        this.so_thu_hai = scanner.nextInt();
+    }
+
+    public int tinhGiaiThua(){
+        int n = this.so_can_tinh;
+        int gt = 1;
+        for (int i=1;i<=n;i++){
+            gt*=i;
+        }
+        return  gt;
+    }
+
+    public int tinhTong(){
+        return this.so_thu_nhat + this.so_thu_hai;
+    }
+
+}
