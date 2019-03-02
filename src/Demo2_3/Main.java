8246d92013aee5ee4bbdffaf83c9d86737d55c4c
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-03-02 20:27:30 +0700

    demo stream - collection

--- src/Demo2_3/Main.java
+++ src/Demo2_3/Main.java
@@ -1,76 +1,97 @@
 package Demo2_3;
 
 import java.util.*;
+import java.util.stream.Stream;
+
 import Ass8.*;
 public class Main {
     public static void main(String[] args){
-        int x = -2;
-        int y = Math.abs(x);
-        System.out.println(y);
+//        int x = -2;
+//        int y = Math.abs(x);
+//        System.out.println(y);
+//
+//        String str = "hello world";
+//        System.out.println(str.toUpperCase());
+//
+//        String[] arr = str.split(" ");
+//
+//        System.out.println((int)Math.floor(3.1415));
+//        System.out.println(Math.ceil(3.1415));
+//        System.out.println(Math.round(3.1415));
+//
+//        ArrayList<String> stringArrayList = new ArrayList<>();
+//        stringArrayList.add("a");
+//        stringArrayList.add("b");
+//        stringArrayList.add("c");
+//        stringArrayList.add("d");
+//
+//        for (String stri: stringArrayList){
+//            System.out.println(stri);
+//        }
+//
+//        Iterator itr = stringArrayList.iterator();
+//
+//        while (itr.hasNext()){
+//            String e = (String)itr.next();
+//            if(e.equals("b")){
+//               // itr.remove();
+//            }else {
+//                System.out.println(e);
+//            }
+//        }
+//
+//        List<String> stringArrayList1 = stringArrayList.subList(0,2);
+//        for (String stri:stringArrayList1){
+//            System.out.println(stri);
+//        }
+//
+//        System.out.println(stringArrayList.indexOf("d"));
+//
+//
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        HashMap inputData = new HashMap();
+//
+//        inputData.put("ID",scanner.nextInt());
+//        inputData.put("name",scanner.nextLine());
+//        inputData.put("age",scanner.nextInt());
+//        inputData.put("address",scanner.nextLine());
+//       // inputData.put("gpa",scanner.nextFloat());
+//
+//    Student student = new Student((int)inputData.get("ID"),(String)inputData.get("name"),
+//        (int)inputData.get("age"),(String)inputData.get("address"),
+//            (inputData.containsKey("gpa")?(float)inputData.get("gpa"):0) );
+//
+//
+//        ArrayList<HashMap> hashMapArrayList= new ArrayList<>();
+//        for (int i=0;i<10;i++){
+//            inputData.put("ID",scanner.nextInt());
+//            inputData.put("name",scanner.nextLine());
+//            inputData.put("age",scanner.nextInt());
+//            inputData.put("address",scanner.nextLine());
+//            inputData.put("gpa",scanner.nextFloat());
+//            hashMapArrayList.add(inputData);
+//        }
 
-        String str = "hello world";
-        System.out.println(str.toUpperCase());
+        ArrayList<String> arrString = new ArrayList<>();
+        arrString.add("a");
+        arrString.add("aa");
+        arrString.add("c");
+        arrString.add("cd");
+        arrString.add("b");
+        arrString.add("bb");
+        arrString.add("abc");
 
-        String[] arr = str.split(" ");
-
-        System.out.println((int)Math.floor(3.1415));
-        System.out.println(Math.ceil(3.1415));
-        System.out.println(Math.round(3.1415));
-
-        ArrayList<String> stringArrayList = new ArrayList<>();
-        stringArrayList.add("a");
-        stringArrayList.add("b");
-        stringArrayList.add("c");
-        stringArrayList.add("d");
-
-        for (String stri: stringArrayList){
-            System.out.println(stri);
-        }
-
-        Iterator itr = stringArrayList.iterator();
-
-        while (itr.hasNext()){
-            String e = (String)itr.next();
-            if(e.equals("b")){
-               // itr.remove();
-            }else {
-                System.out.println(e);
-            }
-        }
-
-        List<String> stringArrayList1 = stringArrayList.subList(0,2);
-        for (String stri:stringArrayList1){
-            System.out.println(stri);
-        }
-
-        System.out.println(stringArrayList.indexOf("d"));
-
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        HashMap inputData = new HashMap();
-
-        inputData.put("ID",scanner.nextInt());
-        inputData.put("name",scanner.nextLine());
-        inputData.put("age",scanner.nextInt());
-        inputData.put("address",scanner.nextLine());
-       // inputData.put("gpa",scanner.nextFloat());
-
-    Student student = new Student((int)inputData.get("ID"),(String)inputData.get("name"),
-        (int)inputData.get("age"),(String)inputData.get("address"),
-            (inputData.containsKey("gpa")?(float)inputData.get("gpa"):0) );
-
-
-        ArrayList<HashMap> hashMapArrayList= new ArrayList<>();
-        for (int i=0;i<10;i++){
-            inputData.put("ID",scanner.nextInt());
-            inputData.put("name",scanner.nextLine());
-            inputData.put("age",scanner.nextInt());
-            inputData.put("address",scanner.nextLine());
-            inputData.put("gpa",scanner.nextFloat());
-            hashMapArrayList.add(inputData);
-        }
+        arrString.stream()
+                .map(s -> {return s.toUpperCase();})
+                .sorted(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return o2.compareTo(o1);
+                    }
+                })
+                .forEach(s -> System.out.println(s));
 
     }
 }
