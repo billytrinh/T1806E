ec0f6e62621fa2984c8e75fe5d1977e548568655
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-03-07 18:45:03 +0700

    Ass1 (ADF2)

---
+++ src/Session_3/SubThread.java
@@ -0,0 +1,16 @@
+package Session_3;
+
+public class SubThread extends Thread{
+
+    public void run(){
+        for (int i=0;i<10;i++){
+            try {
+                System.out.println("    Running in sub thread --"+i);
+                Thread.sleep(1000);
+            }catch (InterruptedException e){}
+
+        }
+
+    }
+
+}
